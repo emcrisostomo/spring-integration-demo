8843f3ee245c2ec2acd1aebb0cf8af7897208960
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-02-09 19:43:44 +0100

    Define exchange and bindings

--- spring-integration-rabbitmq-producer-demo/src/main/java/com/accenture/springintegrationrabbitmqdemo/integration/ApiMutuaOutboundFlowConfiguration.java
+++ spring-integration-rabbitmq-producer-demo/src/main/java/com/accenture/springintegrationrabbitmqdemo/integration/ApiMutuaOutboundFlowConfiguration.java
@@ -1,5 +1,9 @@
 package com.accenture.springintegrationrabbitmqdemo.integration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +12,31 @@ import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Transformers;
 
-import static com.accenture.springintegrationrabbitmqdemo.integration.Channels.API_MUTUA_INPUT_CHANNEL;
+import static com.accenture.springintegrationrabbitmqdemo.integration.Channels.*;
 
 @Configuration
 public class ApiMutuaOutboundFlowConfiguration
 {
     @Autowired
     private RabbitMqConfiguration rabbitConfig;
+
+    @Bean
+    public DirectExchange mutuasInputExchange()
+    {
+        return new DirectExchange(MUTUAS_INPUT_QUEUE_NAME, true, false);
+    }
+
+    @Bean
+    public Queue mutuasQueue()
+    {
+        return new Queue(MUTUAS_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Binding mutuasInputExchangeBinding(Queue queue, DirectExchange directExchange)
+    {
+        return BindingBuilder.bind(queue).to(directExchange).with("");
+    }
 
     @Bean
     public IntegrationFlow apiMutuaInputFlow()
