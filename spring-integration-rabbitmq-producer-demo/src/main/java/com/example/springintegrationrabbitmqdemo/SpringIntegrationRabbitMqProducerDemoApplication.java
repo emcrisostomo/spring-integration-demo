3b635d86263c8c49599ce162329caf7d4641ebde
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-02-10 22:03:03 +0100

    Rename to match project name

--- spring-integration-rabbitmq-producer-demo/src/main/java/com/example/springintegrationrabbitmqdemo/SpringIntegrationRabbitMqProducerDemoApplication.java
+++ spring-integration-rabbitmq-producer-demo/src/main/java/com/example/springintegrationrabbitmqdemo/SpringIntegrationRabbitMqProducerDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringIntegrationRabbitMqDemoApplication
+public class SpringIntegrationRabbitMqProducerDemoApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(SpringIntegrationRabbitMqDemoApplication.class, args);
+        SpringApplication.run(SpringIntegrationRabbitMqProducerDemoApplication.class, args);
     }
 }
