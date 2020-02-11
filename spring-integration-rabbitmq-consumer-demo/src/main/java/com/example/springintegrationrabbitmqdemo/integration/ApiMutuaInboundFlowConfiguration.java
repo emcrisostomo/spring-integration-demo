fc4c5c3082ba5d8f8fbba66d13fd9d216ed6f43a
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-02-12 00:05:49 +0100

    Declare the queue and the binding in the consumer

--- spring-integration-rabbitmq-consumer-demo/src/main/java/com/example/springintegrationrabbitmqdemo/integration/ApiMutuaInboundFlowConfiguration.java
+++ spring-integration-rabbitmq-consumer-demo/src/main/java/com/example/springintegrationrabbitmqdemo/integration/ApiMutuaInboundFlowConfiguration.java
@@ -1,5 +1,9 @@
 package com.example.springintegrationrabbitmqdemo.integration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.integration.amqp.dsl.Amqp;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 
+import static com.example.springintegrationrabbitmqdemo.integration.Channels.MUTUAS_INPUT_QUEUE_NAME;
 import static com.example.springintegrationrabbitmqdemo.integration.Channels.MUTUAS_QUEUE_NAME;
 
 @Configuration
@@ -19,6 +24,24 @@ public class ApiMutuaInboundFlowConfiguration
 
     @Autowired
     private ExampleSubscriberService exampleSubscriberService;
+
+    @Bean
+    public DirectExchange mutuasInputExchange()
+    {
+        return new DirectExchange(MUTUAS_INPUT_QUEUE_NAME, true, false);
+    }
+
+    @Bean
+    public Queue mutuasQueue()
+    {
+        return new Queue(MUTUAS_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Binding mutuasInputExchangeBinding(Queue queue, DirectExchange directExchange)
+    {
+        return BindingBuilder.bind(queue).to(directExchange).with("");
+    }
 
     @Bean
     public SimpleMessageListenerContainer workListenerContainer()
--- spring-integration-rabbitmq-consumer-demo/src/main/java/com/example/springintegrationrabbitmqdemo/integration/Channels.java
+++ spring-integration-rabbitmq-consumer-demo/src/main/java/com/example/springintegrationrabbitmqdemo/integration/Channels.java
@@ -2,6 +2,7 @@ package com.example.springintegrationrabbitmqdemo.integration;
 
 public final class Channels
 {
+    public static final String MUTUAS_INPUT_QUEUE_NAME = "mutuas.input.exchange";
     public static final String MUTUAS_QUEUE_NAME = "mutuas";
 
     private Channels()
